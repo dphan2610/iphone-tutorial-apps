f28eff77be4bbae895b548e6b823f568ecbaa710
dphan2610 <dphan2610@gmail.com>
2018-04-23 10:28:43 -0400

    Add files via upload

---
+++ RandomColor/RandomColor/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  RandomColor
+//
+//  Created by stars on 10/10/13.
+//  Copyright (c) 2013 stars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "View1.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) View1 *view;
+
+@end
---
+++ RandomColor/RandomColor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RandomColor
+//
+//  Created by stars on 10/10/13.
+//  Copyright (c) 2013 stars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
